e589317fdadbf85349e22e50d63c37701123ffd2
James Brooking <jambroo@gmail.com>
2017-09-20 16:51:55 +0100

    PreviousTrips component added.

---
+++ front/app/components/PreviousTrips.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Heading from './Heading'
+import Location from './Location'
+
+import './PreviousTrips.scss'
+
+const PreviousTrips = ({ trips }) => (
+  <div>
+    <Heading>Previous Trips</Heading>
+    <ul className="PreviousTrips">
+      {trips.length === 0 && <i>No trips recorded.</i>}
+      {trips.map(trip => (
+        <li key={trip.id}>
+          <Location>{trip.src}</Location>
+          <i className="material-icons Arrow">chevron_right</i>
+          <Location>{trip.dest}</Location>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+PreviousTrips.propTypes = {
+  trips: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      src: PropTypes.string.isRequired,
+      dest: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
+
+export default PreviousTrips
