0ba17ea7f29f27e182b7bf8d873e8908339ac54e
James Brooking <jambroo@gmail.com>
2017-10-16 10:58:14 +0100

    pyramid talks to db now.

--- pyramid/main.py
+++ pyramid/main.py
@@ -1,11 +1,23 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import psycopg2
+import os
 
 def trips(request):
     # TODO: implement list trips here
+    return Response("...")
 
 if __name__ == '__main__':
+    conn_string = "host='172.17.0.1' dbname='worlds_collide' user='"+ \
+        os.environ['PG_USER']+"' password='"+os.environ['PG_USER']+"'"
+
+    conn = psycopg2.connect(conn_string)
+
+    print("Successfully connecting to database")
+
+    cursor = conn.cursor()
+
     with Configurator() as config:
         config.add_route('trips', '/')
         config.add_view(trips, route_name='trips')
