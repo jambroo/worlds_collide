f205d75ce41277a59a72f47bbb7ff5564a08c73c
James Brooking <jambroo@gmail.com>
2017-10-16 18:03:52 +0100

    adding works.

--- pyramid/main.py
+++ pyramid/main.py
@@ -1,21 +1,28 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.httpexceptions import HTTPBadRequest
 import psycopg2
 import os
 from sqlalchemy import create_engine
 
 from worldscollide.models import DBSession, Trip, Base
 
-def trips(request):
+def trips_list(request):
     trips = []
     for trip in DBSession.query(Trip).all():
         trips.append({"src": trip.src, "dest": trip.dest})
     return Response(str(trips))
 
-def add_trip(request):
-    #DBSession.add(Trip(src="bne", dest="syd"))
-    return Response(True)
+def trips_add(request):
+    if "src" not in request.json_body.keys() or \
+        "dest" not in request.json_body.keys():
+        return HTTPBadRequest("Src and Dest are required input.")
+
+    trip = Trip(src=request.json_body["src"], dest=request.json_body["dest"])
+    result = DBSession.add(trip)
+
+    return Response(result)
 
 if __name__ == '__main__':
     engine = create_engine('postgresql+psycopg2://'+os.environ['PG_USER']+':'+os.environ['PG_PASS']+'@'+os.environ['PG_HOST']+'/worlds_collide')
@@ -24,8 +31,10 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
 
     with Configurator() as config:
-        config.add_route('trips', '/')
-        config.add_view(trips, route_name='trips')
+        config.add_route('trips_list', '/')
+        config.add_route('trips_add', '/add')
+        config.add_view(trips_list, route_name='trips_list')
+        config.add_view(trips_add, route_name='trips_add')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
