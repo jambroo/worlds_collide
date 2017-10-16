da7da8b7eda1434760b53e0d1992f49f3c37636c
James Brooking <jambroo@gmail.com>
2017-10-16 16:28:59 +0100

    Dockerfile added for pyramid.

---
+++ pyramid/worldscollide/models.py
@@ -0,0 +1,29 @@
+from pyramid.security import Allow, Everyone
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+    )
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+Base = declarative_base()
+
+class Trip(Base):
+    __tablename__ = 'trips'
+    uid = Column(Integer, primary_key=True)
+    src = Column(String(100))
+    dest = Column(String(100))
+
+
+class Root(object):
+    __acl__ = [(Allow, Everyone, 'edit')]
+
+    def __init__(self, request):
+        pass
