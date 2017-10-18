e0df76f5a4ad961d02a314a057ae153c47e3e807
James Brooking <jambroo@gmail.com>
2017-10-18 22:10:51 +0100

    Add authorization to enable API creation of trips.

--- django/worldscollide/api/resources.py
+++ django/worldscollide/api/resources.py
@@ -1,7 +1,9 @@
 from tastypie.resources import ModelResource
 from api.models import Trip
+from tastypie.authorization import Authorization
 
 class TripResource(ModelResource):
     class Meta:
         queryset = Trip.objects.all()
         resource_name = 'trip'
+        authorization = Authorization()
