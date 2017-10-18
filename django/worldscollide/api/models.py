e3f4ac88afc6bf07bcf11a82df23d286170c515c
James Brooking <jambroo@gmail.com>
2017-10-18 21:37:09 +0100

    API app added to django.

---
+++ django/worldscollide/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Trip(models.Model):
+    src = models.CharField(max_length=100)
+    dest = models.CharField(max_length=100)
+
+def __str__(self):
+    return "sup"
