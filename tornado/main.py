2e467e8e75ffea3ac0c25032d89663209b848a37
James Brooking <jambroo@gmail.com>
2017-10-20 12:43:08 +0100

    Added main tornado file that outputs routes while I work on them.

---
+++ tornado/main.py
@@ -0,0 +1,21 @@
+import json
+import tornado.ioloop
+from tornado_json.routes import get_routes
+from tornado_json.application import Application
+
+
+PORT = 3004
+
+if __name__ == "__main__":
+  import worldscollide
+
+  routes = get_routes(worldscollide)
+
+  print("Routes\n======\n\n" + json.dumps(
+      [(url, repr(rh)) for url, rh in routes],
+      indent=2)
+  )
+
+  app = Application(routes=routes, settings={"debug": True})
+  app.listen(PORT)
+  tornado.ioloop.IOLoop.current().start()
